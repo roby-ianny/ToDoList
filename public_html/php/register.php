2af71d300a37cd2fbf1719413c909328b1ddd788
Roberto Pio Iannello <s5201538@studenti.unige.it>
2024-09-03 16:45:54 +0200

    registrazione di base funzionante

--- public_html/php/register.php
+++ public_html/php/register.php
@@ -8,55 +8,78 @@
 <body>
 
 <?php
-$firstname = trim(htmlspecialchars($_POST["firstname"], ENT_SUBSTITUTE, null));
-$lastname = trim(htmlspecialchars($_POST["lastname"], ENT_SUBSTITUTE, null));
-$mail = filter_var($_POST["email"], FILTER_SANITIZE_EMAIL);
-$pass = $_POST["pass"];
-$confirm = $_POST["confirm"];
 
-$errors = [];
+  if ($_SERVER["REQUEST_METHOD"] != "POST") {
+      echo "Invalid request";
+      exit;
+  }
+  
 
-if (!$firstname) {
-    $errors[] = "Invalid name";
-}
 
-if (!$lastname) {
+  $errors = [];
+
+  if ($_POST["firstname"]) {
+      $firstname = trim(htmlspecialchars($_POST["firstname"], ENT_SUBSTITUTE, null));
+  } else {
+      $errors[] = "Invalid name";
+  }
+
+
+
+  if ($_POST["lastname"]) {
+    $lastname = trim(htmlspecialchars($_POST["lastname"], ENT_SUBSTITUTE, null));
+  } else {
     $errors[] = "Invalid lastname";
-}
+  }
 
-if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-    $errors[] = "Invalid email";
-} else {
-    // TODO: Controlla se l'utente è già registrato
-}
+    if($_POST["mail"]){
+      $mail = filter_var($_POST["mail"], FILTER_SANITIZE_EMAIL);
 
-if ($_POST["pass"] != $_POST["confirm"]) {
-    $errors[] = "Passwords do not match";
-} else {
-    $passwd = password_hash(trim($_POST["pass"]), PASSWORD_DEFAULT);
-}
+      if (!filter_var($mail, FILTER_VALIDATE_EMAIL)) {
+        $errors[] = "Invalid email";
+      }
+    }
 
-if (!$passwd) {
-    $errors[] = "Invalid password";
-}
+    if($_POST["pass"]){
+      $pass = $_POST["pass"];
+    } else {
+        $errors[] = "Invalid password";
+    }
 
-if (empty($errors)) {
-    echo '<p> You can now go to the <a href="./login.html"> login page </a></p>';
+    if($_POST["confirm"]){
+      $confirm = $_POST["confirm"];
+    } else {
+        $errors[] = "Invalid password";
+    }
+
+    if ($pass != $confirm) {
+        $errors[] = "Passwords do not match";
+    } else {
+        $passwd = password_hash(trim($_POST["pass"]), PASSWORD_DEFAULT);
+    }
+
+    if (!$passwd) {
+        $errors[] = "Invalid password";
+    }
+    
+    if (empty($errors)) {
 
     // mi collego al db
     $con = new mysqli("localhost", "root", "", "todolist");
 
-    $stmt = mysqli_prepare(
-        $con,
+    $stmt = $con->prepare(
         "INSERT INTO Users (Firstname, Lastname, Email, Password) VALUES (?, ?, ?, ?)"
     );
 
-    mysqli_stmt_bind_param($stmt, "ss", $firstname, $lastname, $email, $pass);
-    mysqli_exec($con, $stmt);
-} else {
-    echo $errors[0];
-    echo " <a href='./registration.php'>retry </a>";
-}
+    mysqli_stmt_bind_param($stmt, "ssss", $firstname, $lastname, $mail, $passwd);
+    $stmt->execute();
+
+    echo '<p> You can now go to the <a href="./login.html"> login page </a></p>';
+
+    } else {
+      echo $errors[0];
+      echo " <a href='./registration.php'>retry </a>";
+    }
 ?>
 
 </body>
